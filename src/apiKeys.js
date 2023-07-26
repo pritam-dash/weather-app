288ad0964da30fda040c768490994c435bd0fda5
Pritam Dash <pritamdash84@gmail.com>
2023-07-26 22:03:18 +0530

    uploaded

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0940428941d4162f9da20ccff1193ce6",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
